e140f7b2809a2a729444a0982e6dd63430d9d909
mukundtridhya <mukunddtridhyatech@gmail.com>
2024-01-02 16:39:15 +0530

    check apiurl in production

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SocketService } from './services/socket.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -11,5 +12,6 @@ export class AppComponent {
 
   constructor(private socketService: SocketService) {
     this.socketService.startSocket();
+    console.log(environment.apiUrl, 'apiUrl')
   }
 }
