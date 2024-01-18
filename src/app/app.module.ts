09313eb301588e1c0c74ba9fc3e85cbae52ac4b5
mukundtridhya <mukunddtridhyatech@gmail.com>
2024-01-18 15:56:30 +0530

    install pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { MaterialModule } from './shared/material/material.module';
 import { AuthInterceptor } from './auth/http-interceptor/auth.interceptor';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,6 +17,12 @@ import { AuthInterceptor } from './auth/http-interceptor/auth.interceptor';
     BrowserAnimationsModule,
     HttpClientModule,
     MaterialModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
